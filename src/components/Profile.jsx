2c505a6a88529e9048b6477a04aff52e1640b755
maxim <max280596@gmail.com>
2020-12-24 14:04:05 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return  <div className='content'>
+    <div>
+      <img src='https://of-crimea.ru/plug/samye-chistye-plyazhi-1.jpg' alt=''/>
+    </div>
+    <div>Ava + Descr</div>
+    <div>My post
+      <div>New post</div>
+    </div>
+    <div>
+      <div>POst 1</div>
+      <div> post 2</div>
+    </div>
+  
+  </div>
+}
+
+
+export default Profile;
